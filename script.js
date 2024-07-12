7e0728c3345c4bbe4ed9d2f0b8ad2557383eeda1
HalinaTN <tonaganyan.galina@icloud.com>
2024-07-12 17:21:11 +0300

    create script.js

--- script.js
+++ script.js
@@ -7,3 +7,32 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const containerProducts = document.querySelector('.groceries');
+
+
+sendInput.addEventListener('keydown', function(event) {
+    if(event.key == 'Enter') {
+
+    const groceryListText = sendInput.value;
+
+    const newProduct = document.createElement ('div');
+    newProduct.classList.add('products');
+    newProduct.textContent = groceryListText;
+
+    containerProducts.append(newProduct);
+
+    sendInput.value = '';
+
+        const products = document.querySelectorAll('.products');
+
+        for (let item of products) { 
+            item.addEventListener('click', function () {
+              item.classList.toggle('done');
+            })
+        }
+      
+    }
+  })
+
